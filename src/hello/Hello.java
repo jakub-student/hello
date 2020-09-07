c2e8399b7edbe9f0073d1ca5e3fe48dbd01a7789
Jakub Kučera <2017-e-kucera.j@ad.gyarab.cz>
2020-09-07 10:52:59 +0200

    Initial Commit

---
+++ src/hello/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author 2017-e-kucera.j
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Můj skvělý projekt, možná");
+    }
+    
+}
